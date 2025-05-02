bb78ef096821c8d6473eb7de59a502080997f51b
unknown <durgeshsingh12712@gmail.com>
2025-05-02 16:12:51 +0530

    Model trainer added

--- wasteDetection/constants/training_pipeline/__init__.py
+++ wasteDetection/constants/training_pipeline/__init__.py
@@ -21,3 +21,15 @@ DATA_VALIDATION_STATUS_FILE = 'status.txt'
 
 DATA_VALIDATION_ALL_REQUIRED_FILE = ["train", "valid", "data.yaml"]
 
+
+"""
+Model Trainer related constant start with MODEL_TRAINER var name
+"""
+
+MODEL_TRAINER_DIR_NAME: str = "model_trainer"
+
+MODEL_TRAINER_PRETRAINED_WEIGHT_NAME: str = "yolov5s.pt"
+
+MODEL_TRAINER_NO_EPOCHS: int = 1  # If you need good model then increase epochs
+
+MODEL_TRAINER_BATCH_SIZE: int = 16
