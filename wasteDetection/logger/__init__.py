0c20e86534d4ce0c72f69ff572fd3b2a7dc1678f
unknown <durgeshsingh12712@gmail.com>
2025-05-02 13:12:54 +0530

    Data validation

--- wasteDetection/logger/__init__.py
+++ wasteDetection/logger/__init__.py
@@ -11,10 +11,10 @@ log_path = os.path.join(from_root(), 'log', LOG_FILE)
 
 os.makedirs(log_path, exist_ok=True)
 
-lOG_FILE_PATH = os.path.join(log_path, LOG_FILE)
+LOG_FILE_PATH = os.path.join(log_path, LOG_FILE)
 
 logging.basicConfig(
-    filename=lOG_FILE_PATH,
+    filename=LOG_FILE_PATH,
     format= "[ %(asctime)s ] %(name)s - %(levelname)s - %(message)s",
     level= logging.INFO
 )
